3ac3beb0cc01d7eaaf28fe85e59010a70fd21950
Alexander Pechenkin <pechenkin.aa@phystech.edu>
2022-02-01 10:00:40 +0300

    attempt

---
+++ ML/assignment0_04_tree/tree.py
@@ -0,0 +1,357 @@
+import numpy as np
+from sklearn.base import BaseEstimator
+
+
+def entropy(y):  
+    """
+    Computes entropy of the provided distribution. Use log(value + eps) for numerical stability
+    
+    Parameters
+    ----------
+    y : np.array of type float with shape (n_objects, n_classes)
+        One-hot representation of class labels for corresponding subset
+    
+    Returns
+    -------
+    float
+        Entropy of the provided subset
+    """
+    EPS = 0.0005
+    probas = np.mean(y, axis = 0)
+    return -np.sum(probas * np.log(probas + EPS))
+    
+def gini(y):
+    """
+    Computes the Gini impurity of the provided distribution
+    
+    Parameters
+    ----------
+    y : np.array of type float with shape (n_objects, n_classes)
+        One-hot representation of class labels for corresponding subset
+    
+    Returns
+    -------
+    float
+        Gini impurity of the provided subset
+    """
+    probas = np.mean(y, axis = 0)
+    return 1 - np.sum(np.power(probas ,2))
+    
+def variance(y):
+    """
+    Computes the variance the provided target values subset
+    
+    Parameters
+    ----------
+    y : np.array of type float with shape (n_objects, 1)
+        Target values vector
+    
+    Returns
+    -------
+    float
+        Variance of the provided target vector
+    """
+    
+    # YOUR CODE HERE
+    
+    return np.power(y - np.mean(y), 2).mean()
+
+def mad_median(y):
+    """
+    Computes the mean absolute deviation from the median in the
+    provided target values subset
+    
+    Parameters
+    ----------
+    y : np.array of type float with shape (n_objects, 1)
+        Target values vector
+    
+    Returns
+    -------
+    float
+        Mean absolute deviation from the median in the provided vector
+    """
+
+    # YOUR CODE HERE
+    
+    return np.median(np.abs(y - np.median(y)))
+
+
+def one_hot_encode(n_classes, y):
+    y_one_hot = np.zeros((len(y), n_classes), dtype=float)
+    y_one_hot[np.arange(len(y)), y.astype(int)[:, 0]] = 1.
+    return y_one_hot
+
+
+def one_hot_decode(y_one_hot):
+    return y_one_hot.argmax(axis=1)[:, None]
+
+
+class Node:
+    """
+    This class is provided "as is" and it is not mandatory to it use in your code.
+    """
+    def __init__(self, feature_index, threshold, proba=0):
+        self.feature_index = feature_index
+        self.value = threshold
+        self.proba = proba
+        self.left_child = None
+        self.right_child = None
+        
+        
+class DecisionTree(BaseEstimator):
+    all_criterions = {
+        'gini': (gini, True), # (criterion, classification flag)
+        'entropy': (entropy, True),
+        'variance': (variance, False),
+        'mad_median': (mad_median, False)
+    }
+
+    def __init__(self, n_classes=None, max_depth=np.inf, min_samples_split=2, 
+                 criterion_name='gini', debug=False):
+
+        assert criterion_name in self.all_criterions.keys(), 'Criterion name must be on of the following: {}'.format(self.all_criterions.keys())
+        
+        self.n_classes = n_classes
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.criterion_name = criterion_name
+
+        self.depth = 0
+        self.root = None # Use the Node class to initialize it later
+        self.debug = debug
+
+        
+        
+    def make_split(self, feature_index, threshold, X_subset, y_subset):
+        """
+        Makes split of the provided data subset and target values using provided feature and threshold
+        
+        Parameters
+        ----------
+        feature_index : int
+            Index of feature to make split with
+
+        threshold : float
+            Threshold value to perform split
+
+        X_subset : np.array of type float with shape (n_objects, n_features)
+            Feature matrix representing the selected subset
+
+        y_subset : np.array of type float with shape (n_objects, n_classes) in classification 
+                   (n_objects, 1) in regression 
+            One-hot representation of class labels for corresponding subset
+        
+        Returns
+        -------
+        (X_left, y_left) : tuple of np.arrays of same type as input X_subset and y_subset
+            Part of the providev subset where selected feature x^j < threshold
+        (X_right, y_right) : tuple of np.arrays of same type as input X_subset and y_subset
+            Part of the providev subset where selected feature x^j >= threshold
+        """
+
+        X_left = X_subset[X_subset[:,feature_index] < threshold]
+        X_right = X_subset[X_subset[:,feature_index] >= threshold]
+        y_left = y_subset[X_subset[:, feature_index] < threshold]
+        y_right = y_subset[X_subset[:, feature_index] >= threshold]
+        return (X_left, y_left), (X_right, y_right)
+    
+    def make_split_only_y(self, feature_index, threshold, X_subset, y_subset):
+        """
+        Split only target values into two subsets with specified feature and threshold
+        
+        Parameters
+        ----------
+        feature_index : int
+            Index of feature to make split with
+
+        threshold : float
+            Threshold value to perform split
+
+        X_subset : np.array of type float with shape (n_objects, n_features)
+            Feature matrix representing the selected subset
+
+        y_subset : np.array of type float with shape (n_objects, n_classes) in classification 
+                   (n_objects, 1) in regression 
+            One-hot representation of class labels for corresponding subset
+        
+        Returns
+        -------
+        y_left : np.array of type float with shape (n_objects_left, n_classes) in classification 
+                   (n_objects, 1) in regression 
+            Part of the provided subset where selected feature x^j < threshold
+
+        y_right : np.array of type float with shape (n_objects_right, n_classes) in classification 
+                   (n_objects, 1) in regression 
+            Part of the provided subset where selected feature x^j >= threshold
+        """
+
+        y_left = y_subset[X_subset[:, feature_index] < threshold]
+        y_right = y_subset[X_subset[:, feature_index] >= threshold]
+        
+        return y_left, y_right
+
+    def choose_best_split(self, X_subset, y_subset):
+        """
+        Greedily select the best feature and best threshold w.r.t. selected criterion
+        
+        Parameters
+        ----------
+        X_subset : np.array of type float with shape (n_objects, n_features)
+            Feature matrix representing the selected subset
+
+        y_subset : np.array of type float with shape (n_objects, n_classes) in classification 
+                   (n_objects, 1) in regression 
+            One-hot representation of class labels or target values for corresponding subset
+        
+        Returns
+        -------
+        feature_index : int
+            Index of feature to make split with
+
+        threshold : float
+            Threshold value to perform split
+
+        """
+        # YOUR CODE HERE
+        
+        feature_ind = -1
+        best_threshold = 0
+        cur_best = 10e10
+        for feature in range(X_subset.shape[1]):
+            threshold_values = list(set(X_subset[:, feature]))
+            for threshold in threshold_values:
+                y_left, y_right = self.make_split_only_y(feature, threshold, X_subset, y_subset)
+                criteria = self.all_criterions[self.criterion_name][0]
+                cur = (len(y_left) * criteria(y_left) + len(y_right) * criteria(y_right)) / len(y_subset)
+                if cur < cur_best:
+                    cur_best = cur
+                    feature_ind = feature
+                    best_threshold = threshold
+        
+        return feature_ind, best_threshold
+    
+    def make_tree(self, X_subset, y_subset):
+        """
+        Recursively builds the tree
+        
+        Parameters
+        ----------
+        X_subset : np.array of type float with shape (n_objects, n_features)
+            Feature matrix representing the selected subset
+
+        y_subset : np.array of type float with shape (n_objects, n_classes) in classification 
+                   (n_objects, 1) in regression 
+            One-hot representation of class labels or target values for corresponding subset
+        
+        Returns
+        -------
+        root_node : Node class instance
+            Node of the root of the fitted tree
+        """
+
+        # YOUR CODE HERE
+        
+        if self.depth == self.max_depth or X_subset.shape[0] == 1:
+            self.depth -= 1
+            return Node(-1, np.mean(y_subset), np.sum(y_subset, axis=0) / y_subset.shape[0])
+      
+        feature_ind, best_threshold = self.choose_best_split(X_subset, y_subset)
+        (X_left_tr, y_left_tr), (X_right_tr, y_right_tr) = self.make_split(feature_ind, best_threshold, X_subset, y_subset)
+        new_node = Node(feature_ind, best_threshold)
+        self.depth += 1
+        new_node.left_child = self.make_tree(X_left_tr, y_left_tr)
+        self.depth += 1
+        new_node.right_child = self.make_tree(X_right_tr, y_right_tr)
+        self.depth -= 1
+        return new_node
+        
+    def fit(self, X, y):
+        """
+        Fit the model from scratch using the provided data
+        
+        Parameters
+        ----------
+        X : np.array of type float with shape (n_objects, n_features)
+            Feature matrix representing the data to train on
+
+        y : np.array of type int with shape (n_objects, 1) in classification 
+                   of type float with shape (n_objects, 1) in regression 
+            Column vector of class labels in classification or target values in regression
+        
+        """
+        assert len(y.shape) == 2 and len(y) == len(X), 'Wrong y shape'
+        self.criterion, self.classification = self.all_criterions[self.criterion_name]
+        if self.classification:
+            if self.n_classes is None:
+                self.n_classes = len(np.unique(y))
+            y = one_hot_encode(self.n_classes, y)
+
+        self.root = self.make_tree(X, y)
+    
+    def predict(self, X):
+        """
+        Predict the target value or class label  the model from scratch using the provided data
+        
+        Parameters
+        ----------
+        X : np.array of type float with shape (n_objects, n_features)
+            Feature matrix representing the data the predictions should be provided for
+
+        Returns
+        -------
+        y_predicted : np.array of type int with shape (n_objects, 1) in classification 
+                   (n_objects, 1) in regression 
+            Column vector of class labels in classification or target values in regression
+        
+        """
+
+        # YOUR CODE HERE
+        def predict_one(index: int):
+            current_node = self.root
+            while current_node.feature_index != -1:
+                if X[index][current_node.feature_index] < current_node.value:
+                    current_node = current_node.left_child
+                else:
+                    current_node = current_node.right_child 
+            if not self.classification:
+                return current_node.value
+            else:
+                return np.argmax(current_node.proba)
+        
+        y_predicted = np.array([predict_one(i) for i in range(len(X))])
+        return y_predicted
+        
+    def predict_proba(self, X):
+        """
+        Only for classification
+        Predict the class probabilities using the provided data
+        
+        Parameters
+        ----------
+        X : np.array of type float with shape (n_objects, n_features)
+            Feature matrix representing the data the predictions should be provided for
+
+        Returns
+        -------
+        y_predicted_probs : np.array of type float with shape (n_objects, n_classes)
+            Probabilities of each class for the provided objects
+        
+        """
+        assert self.classification, 'Available only for classification problem'
+
+        # YOUR CODE HERE
+        def predict_one(index: int):
+            current_node = self.root
+            while current_node.feature_index != -1:
+                if X[index][current_node.feature_index] < current_node.value:
+                    current_node = current_node.left_child
+                else:
+                    current_node = current_node.right_child 
+            return current_node.proba
+        
+        y_predicted_probs = np.zeros((X.shape[0], self.n_classes))
+        for i in range(X.shape[0]):
+             y_predicted_probs[i, :] = predict_one(i)
+        print(np.abs(y_predicted_probs.mean(axis=0)))
+        return y_predicted_probs
